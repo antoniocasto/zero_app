8d011ad154cc0fe7108984854f366e057e119cac
antoniocasto <antonio9_4@outlook.com>
2020-03-01 12:47:24 +0100

    end lesson 15. Use of Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text(
-        'hello!'
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'My First App'
+          ),
+        ),
       ),
     );
   }
