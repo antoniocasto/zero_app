cc498fafe76fcd8cd513734030b74053b7b2b184
antoniocasto <antonio9_4@outlook.com>
2020-03-01 14:11:18 +0100

    end of lesson 17 - use of columns with raised buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,31 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questions = ['Colore preferito?', 'Animale preferito?'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'My First App'
-          ),
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Risposta 1'),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Risposta 2'),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Risposta 3'),
+            ),
+          ],
         ),
       ),
     );
